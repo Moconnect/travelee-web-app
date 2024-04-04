155d52bd8f0555bd07a9f62a98d4ce6d169efe62
Moses Ekpenisi <moses.ekpenisi@edu.fh-kaernten.ac.at>
2024-04-04 22:52:10 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline uppercase">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque esse voluptatum rerum nisi id reiciendis, ipsam necessitatibus ullam, fuga est ratione reprehenderit quod temporibus quia quisquam quo maxime doloremque saepe.
+    </h1>
+    </div>
+  )
+}
+
+export default App
